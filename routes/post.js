e89fbb5c4da744830ad833117abc86e165f1c139
Srishti Sharma <srishtisharma85253@gmail.com>
2021-12-20 17:15:40 +0530

    feat(): adds allposts route

--- routes/post.js
+++ routes/post.js
@@ -5,6 +5,22 @@ const Post = mongoose.model("Post");
 
 const requireLogin = require("../middleware/requireLogin");
 
+// for getting all posts
+router.get("/allposts", requireLogin, (req, res) => {
+  //  populateBy is a method used to get data instead of ObjectId.
+  //  It allows us to pass the expected field-names as second parameter
+  //  otherwise will return the full record entry
+  Post.find()
+    .populate("postedBy", "_id name")
+    .then((posts) => {
+      return res.json({ posts });
+    })
+    .catch((err) => {
+      return res.json({ error: err });
+    });
+});
+
+// for creating a post
 router.post("/createpost", requireLogin, (req, res) => {
   const { title, description } = req.body;
   if (!title || !description) {
